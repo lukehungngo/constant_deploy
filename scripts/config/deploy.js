f476f102267d11b2db3ee9986c49394bde60bdb6
0xkumi <dung.q@autonomous.nyc>
2019-03-07 18:29:17 +0700

    update

--- scripts/config/deploy.js
+++ scripts/config/deploy.js
@@ -1,4 +1,7 @@
 const node = require("./node")
 var deploy = module.exports
 
-deploy.DO = [node.beacon(0),node.beacon(1),node.beacon(2)]
+deploy.DO = [
+        node.beacon(0,"sgp1"),node.beacon(1,"ams3"),node.beacon(2,"nyc3"), 
+        node.shard(0,0,"sfo2"),node.shard(0,1,"lon1"),node.shard(0,2,"blr1"),
+        node.shard(1,0,"sfo2"),node.shard(1,1,"lon1"),node.shard(1,2,"blr1")]
--- scripts/config/node.js
+++ scripts/config/node.js
@@ -1,15 +1,17 @@
 var node = module.exports
 
-node.beacon = function(id) {
+node.beacon = function(id, region) {
   return {
     name: `beacon-${id}`,
-    tags: ['constant_chain', 'beacon', `beacon-${id}`]
+    tags: ['constant_chain', 'beacon', `beacon-${id}`],
+    region: region
   }
 }
 
-node.shard = function(sid, id) {
+node.shard = function(sid, id, region) {
   return {
-    name: `shard-${id}`,
-    tags: ['constant_chain', 'shard', `shard-${sid}`, `shard-${sid}-${id}`]
+    name: `shard-${sid}-${id}`,
+    tags: ['constant_chain', 'shard', `shard-${sid}`, `shard-${sid}-${id}`],
+    region: region
   }
 }
